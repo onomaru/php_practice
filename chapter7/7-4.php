27010a75c2035799fa8f6f4c4d8b891e893ccc9c
onomaru <ono.810i2@gmail.com>
2020-11-01 00:12:12 +0900

    修正

--- chapter7/7-4.php
+++ chapter7/7-4.php
@@ -1,20 +1,19 @@
 <?php
 
-function sum($n){
-    for($i = 1;$i <= $n;$i++){
+function sum($n)
+{
+    $total = 0;
+    for ($i = 1;$i <= $n;$i++) {
         $total += $i;
     }
     return $total;
-
 }
 
 echo '1からxまでの和を求めます'."\n";
-do{
+do {
     echo 'xの値:';
     $x = intval(trim(fgets(STDIN)));
-}while($x <= 0);
+} while ($x <= 0);
 
 $result = sum($x);
 echo '1から'.$x.'までの和は'.$result.'です'."\n";
-
-?>
